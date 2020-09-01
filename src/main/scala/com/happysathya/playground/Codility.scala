4f578fa3732e02fe2f0e7762d1871067155e377f
Sathyaprakash Dhanabal <sathyaprakash.d@gmail.com>
2020-09-02 00:18:21 +0200

    add isSorted with generics.

--- src/main/scala/com/happysathya/playground/Codility.scala
+++ src/main/scala/com/happysathya/playground/Codility.scala
@@ -77,4 +77,14 @@ object Codility {
     (Range(1, a.length + 2).toSet diff a.toSet).min
   }
 
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    as.foldLeft((true, Option.empty[A]))((lastResult, value) => {
+      lastResult match {
+        case (true, None) => (true, Some(value))
+        case (false, _) => (false, Some(value))
+        case (true, Some(data)) => (ordered(data, value), Some(value))
+      }
+    })._1
+  }
+
 }
--- src/test/scala/com/happysathya/playground/TestPlayground.scala
+++ src/test/scala/com/happysathya/playground/TestPlayground.scala
@@ -175,6 +175,21 @@ class TestPlayground extends FunSuite {
       assert(smallestPositiveInteger(Array(0, -4, -3)) == 1)
       assert(smallestPositiveInteger(Array(2)) == 1)
     }
-
+    {
+      val ordered: (Int, Int) => Boolean = (a: Int, b: Int) => if (b >= a) true else false
+      assert(isSorted(Array(1, 2, 3), ordered))
+      assert(!isSorted(Array(1, 3, 2), ordered))
+      assert(!isSorted(Array(1, 2, 4, 2), ordered))
+      assert(isSorted(Array(10, 21, 21, 22), ordered))
+      assert(isSorted(Array(10, 21, 21, 22), ordered))
+      assert(isSorted(Array.empty[Int], ordered))
+      assert(isSorted(Array(1), ordered))
+    }
+    {
+      val ordered: (String, String) => Boolean = (a: String, b: String) => if (b >= a) true else false
+      assert(isSorted(Array("a", "aa", "aaa"), ordered))
+      assert(isSorted(Array("ab", "ac", "ac", "ad"), ordered))
+      assert(!isSorted(Array("ab", "aa", "ac", "ad"), ordered))
+    }
   }
 }
