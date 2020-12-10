e0d9c0aa332f4acb6286b456803c01b0fa766926
Sathyaprakash Dhanabal <sathyaprakash.d@gmail.com>
2020-12-10 12:59:31 +0100

    Upgrade weaver dependency.

--- src/test/scala/com/happysathya/playground/brise/IncrementNumberTest.scala
+++ src/test/scala/com/happysathya/playground/brise/IncrementNumberTest.scala
@@ -7,15 +7,16 @@ object IncrementNumberTest extends SimpleIOSuite {
 
   simpleTest("should increment sequence by 1") {
     for {
-      a1 <- increment(Seq(5, 4, 3))
+      a1 <- increment(Seq(5, 4, 2))
       a2 <- increment(Seq(9, 9, 9, 9))
       a3 <- increment(Seq())
       a4 <- increment(Seq(1))
       a5 <- increment(Seq(9))
-    } yield expect(a1 == Seq(5, 4, 4)) and
-      expect(a2 == Seq(1, 0, 0, 0, 0)) and
-      expect(a3 == Seq()) and
-      expect(a4 == Seq(2)) and
-      expect(a5 == Seq(1, 0))
+    } yield expect.all(
+      a1 == Seq(5, 4, 4), //expected input must be 5, 4, 3
+      a2 == Seq(1, 0, 0, 0, 0),
+      a3 == Seq(),
+      a4 == Seq(2),
+      a5 == Seq(1, 0))
   }
 }
