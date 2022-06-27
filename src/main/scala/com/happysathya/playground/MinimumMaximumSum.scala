299db2d2b141f5a9d4bc0a0d937047a2f8465786
Sathyaprakash Dhanabal <sathyaprakash.d@gmail.com>
2022-06-27 23:18:41 +0200

    Find minimum maximum sum of positive integers in an array

---
+++ src/main/scala/com/happysathya/playground/MinimumMaximumSum.scala
@@ -0,0 +1,26 @@
+package com.happysathya.playground
+
+import scala.annotation.tailrec
+
+object MinimumMaximumSum {
+
+  def minimumMaximumSum(arr: Array[Int]): (Long, Long) = {
+
+    @tailrec
+    def minimumMaximumSum(arr: Array[Int], sum: Long, minimum: Long, maximum: Long): (Long, Long) = {
+      if (arr.isEmpty)
+        (sum - maximum, sum - minimum)
+      else {
+        val item = arr.head
+        val newMinimum = if (minimum == 0 || item < minimum) item else minimum
+        val newMaximum = if (maximum == 0 || item > maximum) item else maximum
+        minimumMaximumSum(arr.tail, sum + item, newMinimum, newMaximum)
+      }
+    }
+
+    val (min, max) = minimumMaximumSum(arr, 0, 0, 0)
+    println(s"$min $max")
+    (min, max)
+  }
+
+}
---
+++ src/test/scala/com/happysathya/playground/MinimumMaximumTest.scala
@@ -0,0 +1,11 @@
+package com.happysathya.playground
+
+import com.happysathya.playground.MinimumMaximumSum.minimumMaximumSum
+import org.scalatest.funsuite.AnyFunSuite
+
+class MinimumMaximumTest extends AnyFunSuite {
+
+  test("MinimumMaximumTest") {
+    assert(minimumMaximumSum(Array(1, 2, 3, 4, 5)) == (10, 14))
+  }
+}
