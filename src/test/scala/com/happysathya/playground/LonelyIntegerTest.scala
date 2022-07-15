30e1477e098d883b0db249e6bc5d4a67f6be746d
Sathyaprakash Dhanabal <sathyaprakash.d@gmail.com>
2022-07-15 13:25:19 +0200

    scalafmt

--- src/test/scala/com/happysathya/playground/LonelyIntegerTest.scala
+++ src/test/scala/com/happysathya/playground/LonelyIntegerTest.scala
@@ -6,7 +6,7 @@ import org.scalatest.funsuite.AnyFunSuite
 class LonelyIntegerTest extends AnyFunSuite {
 
   test("find lonely number in an array") {
-    assert(lonelyInteger(Array(0,0, 1, 2, 1)) == 2)
+    assert(lonelyInteger(Array(0, 0, 1, 2, 1)) == 2)
     assert(lonelyInteger(Array(1, 1, 2)) == 2)
     assert(lonelyInteger(Array(1)) == 1)
   }
