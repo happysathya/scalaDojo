b9a005a752b875bbe7a10f730580243c12aed587
Sathyaprakash Dhanabal <sathyaprakash.d@gmail.com>
2023-04-03 23:20:21 +0200

    DoublyLinkedList

--- src/test/scala/com/happysathya/playground/LinkedListTest.scala
+++ src/test/scala/com/happysathya/playground/LinkedListTest.scala
@@ -6,10 +6,10 @@ import weaver.FunSuite
 object LinkedListTest extends FunSuite:
 
   test("if item is present in a linked list - Integer") {
-    val node1: DataNode[Int]       = DataNode(10)
-    val node2: DataNode[Int]       = DataNode(20)
-    val node3: DataNode[Int]       = DataNode(30)
-    val node4: LinkedListNode[Int] = EmptyNode
+    val node1: LinkedListDataNode[Int] = LinkedListDataNode(10)
+    val node2: LinkedListDataNode[Int] = LinkedListDataNode(20)
+    val node3: LinkedListDataNode[Int] = LinkedListDataNode(30)
+    val node4: LinkedListNode[Int]     = LinkedListEmptyNode
     val head: LinkedListNode[Int] =
       LinkedList.append(node1, LinkedList.append(node2, LinkedList.append(node3, node4)))
     expect(LinkedList.isPresent(head, 10))
@@ -17,10 +17,10 @@ object LinkedListTest extends FunSuite:
   }
 
   test("if item is present in a linked list - String") {
-    val node1: DataNode[String]       = DataNode("Ten")
-    val node2: DataNode[String]       = DataNode("Twenty")
-    val node3: DataNode[String]       = DataNode("Thirty")
-    val node4: LinkedListNode[String] = EmptyNode
+    val node1: LinkedListDataNode[String] = LinkedListDataNode("Ten")
+    val node2: LinkedListDataNode[String] = LinkedListDataNode("Twenty")
+    val node3: LinkedListDataNode[String] = LinkedListDataNode("Thirty")
+    val node4: LinkedListNode[String]     = LinkedListEmptyNode
     val head: LinkedListNode[String] =
       LinkedList.append(node1, LinkedList.append(node2, LinkedList.append(node3, node4)))
     expect(LinkedList.isPresent(head, "Ten"))
@@ -28,20 +28,20 @@ object LinkedListTest extends FunSuite:
   }
 
   test("fold all items from a linked list - Integer") {
-    val node1: DataNode[Int]       = DataNode(10)
-    val node2: DataNode[Int]       = DataNode(20)
-    val node3: DataNode[Int]       = DataNode(30)
-    val node4: LinkedListNode[Int] = EmptyNode
+    val node1: LinkedListDataNode[Int] = LinkedListDataNode(10)
+    val node2: LinkedListDataNode[Int] = LinkedListDataNode(20)
+    val node3: LinkedListDataNode[Int] = LinkedListDataNode(30)
+    val node4: LinkedListNode[Int]     = LinkedListEmptyNode
     val head: LinkedListNode[Int] =
       LinkedList.append(node1, LinkedList.append(node2, LinkedList.append(node3, node4)))
     expect(LinkedList.fold(head) == List(10, 20, 30))
   }
 
   test("fold all items from a linked list - String") {
-    val node1: DataNode[String]       = DataNode("Ten")
-    val node2: DataNode[String]       = DataNode("Twenty")
-    val node3: DataNode[String]       = DataNode("Thirty")
-    val node4: LinkedListNode[String] = EmptyNode
+    val node1: LinkedListDataNode[String] = LinkedListDataNode("Ten")
+    val node2: LinkedListDataNode[String] = LinkedListDataNode("Twenty")
+    val node3: LinkedListDataNode[String] = LinkedListDataNode("Thirty")
+    val node4: LinkedListNode[String]     = LinkedListEmptyNode
     val head: LinkedListNode[String] =
       LinkedList.append(node1, LinkedList.append(node2, LinkedList.append(node3, node4)))
     expect(LinkedList.fold(head) == List("Ten", "Twenty", "Thirty"))
